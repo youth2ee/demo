1ffe78c8f2fd28a65abb9dbe2bc3b632a6f97352
youth2ee <sprirealee@gmail.com>
2022-05-12 00:47:21 +0900

    Update WebMvcConfig.java

--- demo/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000", "http://prod-todo-frontend.us-west-2.elasticbeanstalk.com")
+//		.allowedOrigins("http://localhost:3000", "http://prod-todo-frontend.us-west-2.elasticbeanstalk.com")
 		.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 		.allowedHeaders("*")
 		.allowCredentials(true)
